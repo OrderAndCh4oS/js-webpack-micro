aace249cde960d4657e0dc289b7526b04c559fc0
sarcoma <sarcoma@live.co.uk>
2018-03-31 23:37:22 +0100

    Add some structure

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom'
 
-const Home = () => <h1>Home</h1>;
-const One = () => <h1>One</h1>;
-const Two = () => <h1>Two</h1>;
-const Three = ({match}) => <h1>{match.params.page}</h1>;
+const page = ({title, content}) =>
+    <div className="row">
+        <div className="column col-12">
+            <div>
+                <h1>{title}</h1>
+                <p>{content}</p>
+            </div>
+        </div>
+    </div>
+;
+
+const Home = () => page({title: 'Home', content: 'Intro here'});
+const One = () => page({title: 'One', content: 'Intro here'});
+const Two = () => page({title: 'Two', content: 'Intro here'});
+const Three = ({match}) => page({title: match.params.page, content: 'Intro here'});
 const PageNotFound = () => <h1>Page not found</h1>;
 
 const isActiveFunc = (match) => {
@@ -12,7 +23,7 @@ const isActiveFunc = (match) => {
 };
 
 const Links = () => (
-    <nav>
+    <nav className="main-nav">
         <NavLink exact isActive={isActiveFunc} activeClassName="active" className="button" to="/">Home</NavLink>
         <NavLink strict isActive={isActiveFunc} activeClassName="active" className="button" to="/one">One</NavLink>
         <NavLink strict isActive={isActiveFunc} activeClassName="active" className="button" to="/two">Two</NavLink>
@@ -24,14 +35,22 @@ const Links = () => (
 const Navigation = () => (
     <Router>
         <div className="page-content">
-            <Links/>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route strict path="/one" component={One}/>
-                <Route strict path="/two" component={Two}/>
-                <Route strict path="/three/:page" component={Three}/>
-                <Route strict component={PageNotFound}/>
-            </Switch>
+            <div className="container">
+                <div className="row">
+                    <div className="column col-12">
+                        <Links/>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route strict path="/one" component={One}/>
+                    <Route strict path="/two" component={Two}/>
+                    <Route strict path="/three/:page" component={Three}/>
+                    <Route strict component={PageNotFound}/>
+                </Switch>
+            </div>
         </div>
     </Router>
 );
