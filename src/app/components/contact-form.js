cdc26ed082fd009b49d54a933f31c1e99250b313
sarcoma <sarcoma@live.co.uk>
2018-07-15 19:19:57 +0100

    Add redux-form validation to contact form example

--- src/app/components/contact-form.js
+++ src/app/components/contact-form.js
@@ -2,26 +2,64 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
+const validate = values => {
+    const errors = {};
+    if (!values.fullname) {
+        errors.fullname = 'Required';
+    }
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email';
+    }
+    if (!values.message) {
+        errors.message = 'Required';
+    }
+    return errors;
+};
+
+const renderInput = ({input, label, type, meta: {touched, error}}) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} type={type} placeholder={label}
+                   className={[touched && error ? 'form-error' : ''].join(' ')}/>
+            {touched && error && <p className='form-error'>{error}</p>}
+        </div>
+    </div>
+);
+
+const renderTextArea = ({input, label, meta: {touched, error}}) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <textarea {...input} placeholder={label} className={[touched && error ? 'form-error' : ''].join(' ')}/>
+            {touched && error && <p className='form-error'>{error}</p>}
+        </div>
+    </div>
+);
+
 let ContactForm = ({handleSubmit}) =>
     <form onSubmit={handleSubmit}>
         <div>
-            <label htmlFor="firstName">First Name</label>
-            <Field name="firstName" component="input" type="text"/>
-        </div>
-        <div>
-            <label htmlFor="lastName">Last Name</label>
-            <Field name="lastName" component="input" type="text"/>
+            <label htmlFor="fullname">First Name</label>
+            <Field name="fullname" component={renderInput} type="text"/>
         </div>
         <div>
             <label htmlFor="email">Email</label>
-            <Field name="email" component="input" type="email"/>
+            <Field name="email" component={renderInput} type="email"/>
+        </div>
+        <div>
+            <label htmlFor="message">Message</label>
+            <Field name="message" component={renderTextArea}/>
         </div>
         <button type="submit">Submit</button>
     </form>
 ;
 
 ContactForm = reduxForm({
-    form: 'contact'
+    form: 'contact',
+    validate
 })(ContactForm);
 
 export default ContactForm;
